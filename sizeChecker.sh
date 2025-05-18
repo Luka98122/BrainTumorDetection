7251114308dd82bd63addee81f5b63a203b10d92
Luka Markovic <luka98122@gmail.com>
2025-05-18 11:40:04 +0200

    Modified dataset

--- sizeChecker.sh
+++ sizeChecker.sh
@@ -9,8 +9,17 @@ while IFS= read -r -d '' file; do
     # Use `file` to get image info
     info=$(file "$file")
     
-    # Extract dimensions (e.g., 512 x 512)
+    # Extract dimensions (e.g., 256 x 256)
     if [[ $info =~ ([0-9]+)\ x\ ([0-9]+) ]]; then
-        echo "${BASH_REMATCH[1]}x${BASH_REMATCH[2]}"
+        width=${BASH_REMATCH[1]}
+        height=${BASH_REMATCH[2]}
+        dimension="${width}x${height}"
+        echo "$dimension"
+        
+        # Delete if dimensions are 256x256
+        if [[ "$dimension" == "256x256" ]]; then
+            echo "Deleting $file"
+            rm "$file"
+        fi
     fi
 done | sort | uniq -c | awk '{print $2 " - " $1}'
